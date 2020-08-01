3668c37014f1ce0a392acb3017e10b33d775551f
Cristinacoronado <Cristinacoronado68@gmail.com>
2020-08-01 15:02:27 -0400

    10 prints and new class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main{
-	public static void main(String[]arsg){
+	public static void main(String[]args){
+		for(int i = 0; i < 10; i++){
+		System.out.println("Hello, life!");
 	}
+  }	
 }
